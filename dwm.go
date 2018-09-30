fb3c7c2e50d9f7cad5a3d19cb7deb7c6507e1ff5
wiless <wiless.bytes@gmail.com>
2018-09-30 11:51:39 +0530

    Added EUID, TXPOWER and few

--- dwm.go
+++ dwm.go
@@ -1,6 +1,7 @@
 package dwm
 
 import (
+	"encoding/binary"
 	"fmt"
 	"log"
 
@@ -8,7 +9,8 @@ import (
 )
 
 type DWM1000 struct {
-	dev *spi.Device
+	dev   *spi.Device
+	devID DevID
 }
 
 func (d *DWM1000) Init() error {
@@ -32,6 +34,26 @@ type DevID struct {
 	RIDTAG, MODEL, VER, REV int
 }
 
+func (d *DevID) Fill(data []byte) {
+	d.RIDTAG = int(binary.BigEndian.Uint16(data[0:2]))
+	fmt.Printf("\n INT Value %d \t %0X", d.RIDTAG, d.RIDTAG)
+	d.MODEL = int(uint8(data[2]))
+	fmt.Printf("\n INT Value %d ", d.MODEL)
+
+	d.VER = int(uint8((data[3] & 0xF0) >> 4))
+	d.REV = int(uint8(data[3] & 0x0F))
+	fmt.Printf("\n MODEL Value %d ", d.MODEL)
+	fmt.Printf("\n VER Value %d ", d.VER)
+	fmt.Printf("\n REV Value %d ", d.REV)
+	// fmt.Printf("DEVICE %#v", d)
+	// fmt.Printf("DEVICE %v", d)
+}
+
+func (d *DevID) String() string {
+
+	return fmt.Sprintf("%0x,Model:%d,Ver:%v,Rev:%v", d.RIDTAG, d.MODEL, d.VER, d.REV)
+}
+
 const NOP byte = 0X00
 
 func (d *DWM1000) DeviceID() string {
@@ -42,17 +64,72 @@ func (d *DWM1000) DeviceID() string {
 	if error != nil {
 		log.Println("Error reading REG ")
 	}
-	id := flip(output[1:])
+	fmt.Printf("\n raw DEV_ID Response : %0x \n", output)
 
-	fmt.Printf("\n DEV_ID Response : %x \n", id)
-	return fmt.Sprintf("%x", id)
+	id := flip(output[1:])
+	fmt.Printf("\n DEV_ID Response : %0x \n", id)
+	d.devID.Fill(id)
+	return fmt.Sprintf("%0X", id)
+}
+
+func (d *DWM1000) DevID() DevID {
+	return d.devID
 }
 
 func flip(data []byte) []byte {
 	N := len(data)
+	fmt.Printf("\nRecieved .. %0X", data)
 	result := make([]byte, len(data))
 	for i, d := range data {
 		result[N-i-1] = d
 	}
+	fmt.Printf("\nReturning .. %0X", result)
 	return result
+}
+
+func (d *DWM1000) EUI_ID() string {
+
+	cmd := []byte{0x01, NOP, NOP, NOP, NOP, NOP, NOP, NOP, NOP}
+	output := make([]byte, len(cmd))
+	error := d.dev.Tx(cmd, output)
+	if error != nil {
+		log.Println("Error reading REG ")
+	}
+	fmt.Printf("\n raw EUI ID Response : %0x \n", output)
+
+	id := flip(output[1:])
+	fmt.Printf("\n EUI ID Response : %0x \n", id)
+
+	return fmt.Sprintf("%0X", id)
+}
+
+func (d *DWM1000) TxPower() string {
+	// 0x1E
+	cmd := []byte{0x1E, NOP, NOP, NOP, NOP}
+	output := make([]byte, len(cmd))
+	error := d.dev.Tx(cmd, output)
+	if error != nil {
+		log.Println("Error reading REG ")
+	}
+	fmt.Printf("\n raw TxPower Response : %0x \n", output)
+
+	id := flip(output[1:])
+	fmt.Printf("\n TxPower Response : %0x \n", id)
+
+	return fmt.Sprintf("%0x", id)
+}
+func (d *DWM1000) PANid() string {
+	// 0x1E
+	cmd := []byte{0x03, NOP, NOP, NOP, NOP}
+	output := make([]byte, len(cmd))
+	error := d.dev.Tx(cmd, output)
+	if error != nil {
+		log.Println("Error reading REG ")
+	}
+	fmt.Printf("\n raw PAN Response : %0x \n", output)
+
+	id := flip(output[1:])
+	fmt.Printf("\n PAN Response : %0x \n", id)
+
+	return fmt.Sprintf("%0x", id)
 }
--- examples/main.go
+++ examples/main.go
@@ -1,10 +1,19 @@
 package main
 
-import dwm "github.com/hi-manshu/DWM1000"
+import (
+	"fmt"
+
+	dwm "github.com/hi-manshu/DWM1000"
+)
 
 func main() {
 
 	var tag dwm.DWM1000
 	tag.Init()
-	tag.DeviceID()
+	fmt.Println(tag.DeviceID())
+
+	fmt.Println(tag.EUI_ID())
+	fmt.Println(tag.TxPower())
+
+	fmt.Println(tag.PANid())
 }
